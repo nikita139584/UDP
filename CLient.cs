7f25c15042baeac61a01e04da2edd5b3e23d6957
nikita139584 <staskonikita139@gmail.com>
2026-05-04 22:06:39 +0200

    Add files via upload

---
+++ CLient.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    private const int PORT = 27015;
+
+    static async Task Main()
+    {
+        var udp = new UdpClient();
+
+
+        var server = new IPEndPoint(IPAddress.Parse("127.0.0.1"), PORT);
+
+        Console.WriteLine("Клієнт запущено");
+
+  
+        _ = Task.Run(async () =>
+        {
+            while (true)
+            {
+                var result = await udp.ReceiveAsync();
+                var msg = Encoding.UTF8.GetString(result.Buffer);
+                Console.WriteLine(msg);
+            }
+        });
+
+        while (true)
+        {
+            var text = Console.ReadLine();
+            var data = Encoding.UTF8.GetBytes(text!);
+            await udp.SendAsync(data, data.Length, server);
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    private const int PORT = 27015;
+    private static List<IPEndPoint> clients = new List<IPEndPoint>();
+
+    static async Task Main()
+    {
+        var udp = new UdpClient(PORT);
+        Console.WriteLine("UDP сервер запущено");
+
+        while (true)
+        {
+            var result = await udp.ReceiveAsync();
+            var message = Encoding.UTF8.GetString(result.Buffer);
+            var sender = result.RemoteEndPoint;
+
+            Console.WriteLine($"[{sender}] {message}");
+
+
+            if (!clients.Any(c => c.Equals(sender)))
+                clients.Add(sender);
+
+            var data = Encoding.UTF8.GetBytes($"{sender}: {message}");
+
+    
+            foreach (var client in clients)
+            {
+                await udp.SendAsync(data, data.Length, client);
+            }
+        }
+    }
+}
